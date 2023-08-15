27286e028a2462f1658e760eb9a6949710c35117
Azar867 <52555238+Azar867@users.noreply.github.com>
2023-08-15 20:57:57 +0530

    removed print statements

--- app.py
+++ app.py
@@ -24,25 +24,16 @@ def chat():
 
 
 def get_Chat_response(text):
-    print("text :: "+text)
-    print("tokenizer :: ",tokenizer)
-    print("model :: ",model)
     # Let's chat for 5 lines
     for step in range(5):
-        print("tokenizer.eos_token :: ",tokenizer.eos_token)
-        print("tokenizer.eos_token_id :: ",tokenizer.eos_token_id)
         # encode the new user input, add the eos_token and return a tensor in Pytorch
         new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-        print("step :: ",step)
-        print("new_user_input_ids :: ",new_user_input_ids)
 
         # append the new user input tokens to the chat history
         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-        print("bot_input_ids :: ",bot_input_ids)
 
         # generated a response while limiting the total chat history to 1000 tokens, 
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        print("chat_history_ids :: ",chat_history_ids)
 
         # pretty print last ouput tokens from bot
         return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
